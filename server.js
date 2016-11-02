a43987ed5f1f6033a6ae744de40c3b42a83214e5
fresh5447 <doug@kosmojo.com>
2016-11-01 19:53:37 -0600

    setup seperate apps for client and server, proxy server api to be served to client, absolutely bojangled on why api port is 3100

---
+++ server.js
@@ -0,0 +1,23 @@
+const path           = require('path'),
+  express             = require('express'),
+  bodyParser          = require('body-parser'),
+  app                 = express();
+
+  // Express only serves static assets in production
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  }
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/home', function(req, res) {
+  res.json({ message: "hi there!" })
+})
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, function(){
+  console.log("🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 fired up 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 \n🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥 on " + port + " 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥\n🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥")
+});
