d622a5616d5707448659478d8f77e0e80831fe2f
fresh5447 <doug@kosmojo.com>
2017-02-01 11:57:14 -0700

    make my courses view a little more attractive

--- client/src/BSCA/Home/HomeContainer.js
+++ client/src/BSCA/Home/HomeContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import NavLink from '../../Components/NavLink';
 import $ from 'jquery';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron, Panel } from 'react-bootstrap';
+import Spinner from 'react-icons/lib/fa/arrow-right';
 
 class HomeContainer extends Component {
   constructor(props, context) {
@@ -31,15 +32,24 @@ class HomeContainer extends Component {
   }
   render() {
     const availableCourses = this.state.courses ? this.state.courses.map((item) => {
-      return <li> <NavLink to={'/big-sky-code-academy/course/' + item._id}> {item.title}</NavLink> </li>
+      const footer = (
+          <span className="res-footer res-courses flex-cats">
+            <NavLink to={'/big-sky-code-academy/course/' + item._id}> <Spinner className="right-arrow"/> </NavLink>
+          </span>
+      )
+      return (<Panel header={item.title} footer={footer}>
+                {item.desc}
+              </Panel>
+          )
     }) : null
     return (
     <div>
       <Jumbotron className="student-jumbo">
-        <h3>Enrolled Courses</h3>
-        <ul>
+        <h3 className="my-courses">My Courses</h3>
+        <div>
           { availableCourses }
-        </ul>
+        </div>
+
       </Jumbotron>
     </div>
     )
