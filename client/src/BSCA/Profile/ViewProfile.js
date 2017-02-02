15b9c799690a62fee9899d5e80477b4e866fd66a
fresh5447 <doug@kosmojo.com>
2017-02-02 16:59:21 -0700

    build for heroku

--- client/src/BSCA/Profile/ViewProfile.js
+++ client/src/BSCA/Profile/ViewProfile.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Jumbotron, Button } from 'react-bootstrap';
+import { Jumbotron } from 'react-bootstrap';
 import NavLink from '../../Components/NavLink';
+import Tweeter from 'react-icons/lib/fa/twitter';
+import Github from 'react-icons/lib/fa/github';
+import Linkedin from 'react-icons/lib/fa/linkedin-square';
+import Blog from 'react-icons/lib/fa/bullhorn';
+import Website from 'react-icons/lib/fa/user';
+import Email from 'react-icons/lib/fa/envelope-o'
 
 const ViewProfile = (props) =>
 <Jumbotron>
   <h3>{ props.user.local.firstName + " " + props.user.local.lastName }</h3>
-  <p>{ props.user.local.email }</p>
-  <p>twitter: { props.user.twitter ? props.user.twitter : "please update"}</p>
-  <p>linkedIn: { props.user.linkedIn ? props.user.linkedIn : "please update"}</p>
-  <p>github: { props.user.github ? props.user.github : "please update"}</p>
-  <p>website: { props.user.website ? props.user.website : "please update"}</p>
-  <p>blog: { props.user.blog ? props.user.blog : "please update"}</p>
+  <p> <Email /> { props.user.local.email }</p>
+  <p> <Tweeter /> { props.user.twitter ? props.user.twitter : "add Twitter"}</p>
+  <p> <Linkedin/> { props.user.linkedIn ? props.user.linkedIn : "add LinkedIn"}</p>
+  <p> <Github/> { props.user.github ? props.user.github : "add GitHub"}</p>
+  <p> <Website/> { props.user.website ? props.user.website : "add Website"}</p>
+  <p> <Blog/> { props.user.blog ? props.user.blog : "add Blog"}</p>
 
   <NavLink className="btn btn-primary my-primary-btn" to="/big-sky-code-academy/profile/edit"> Edit </NavLink>
 </Jumbotron>
